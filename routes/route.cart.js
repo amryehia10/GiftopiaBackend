05ea59491430a9a31191a73391d60fa744806ebe
Ahmed Elghabour <ahmedelghabour6@gmail.com>
2024-04-05 12:23:28 +0200

    cart routes

--- routes/route.cart.js
+++ routes/route.cart.js
@@ -2,10 +2,10 @@ const express = require('../packages/node_modules/express');
 const router = express.Router();
 const controller = require("../controllers/controller.cart");
 
-router.get("/:userId", controller.getAllAtCartByUserId);
+router.get("/cart/:userId", controller.getAllAtCartByUserId);
 // router.get("/:id", controller.getCartById);
-router.put("/:id", controller.updateCart);
-router.post("/:userId", controller.addToCart);
+router.put("/cart/:id", controller.updateCart);
+router.post("/cart/:userId", controller.addToCart);
 
 
 
