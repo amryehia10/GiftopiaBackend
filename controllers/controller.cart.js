a061610d0fc447a876e3d956e40664572906dec8
jimmy <devJimmy990@gmail.com>
2024-04-08 13:52:06 +0200

    edit cart

--- controllers/controller.cart.js
+++ controllers/controller.cart.js
@@ -45,27 +45,25 @@ let getUserCart = async (req, res) => {
 }
 let updateCartProducts = async (req, res) => {
     try {
-        const args = req.body;
-        if (validator(args)) {
-            const user = await model.findOne({ userId: args.userId })
-            if (user) {
-                const result = await model.findOneAndUpdate(
-                    { userId: args.userId },
-                    { items: args.items },
-                    { new: true });
-                result ?
-                    res.status(200).json({ status: "success", msg: "Cart Products Updated Successfully ", data: result })
-                    : res.status(404).json({ status: "fail", msg: "No Updates Happened" });
-            } else {
-                const result = await model.create({ ...args })
-                res.status(200).json({ status: "success", msg: "Cart Created Successfully", data: result })
-            }
-        } else {
-            res.status(422).json({ status: "fail", message: validator.errors[0].message });
-        }
-    } catch (error) { res.status(404).json({ status: "fail", error: error.message }) }
-};
-
+      const args = req.body;
+      if (validator(args)) {
+        const user = await model.findOne({ userId: args.userId });
+  
+        const result = user
+          ? await model.findOneAndUpdate({ userId: args.userId }, { $set: args }, { new: true })
+          : await model.create(args);
+  
+        result
+          ? res.status(200).json({ status: "success", msg: "Cart Updated Successfully", data: result })
+          : res.status(404).json({ status: "fail", msg: "No Updates Happened" });
+      } else {
+        res.status(422).json({ status: "fail", message: validator.errors[0].message });
+      }
+    } catch (error) {
+      res.status(400).json({ status: "fail", error: error.message });
+    }
+  };
+  
 module.exports = {
     getAllCarts,
     getUserCart,
@@ -77,27 +75,31 @@ const mapProduct = {
         userId: 1,
         total: 1,
         products: {
-            $map: {
-                input: "$products",
-                as: "product",
-                in: {
-                    _id: "$$product._id",
-                    name: "$$product.name",
-                    price: "$$product.price",
-                    image: { $arrayElemAt: ["$$product.images", 0] },
-                    quantity: "$$product.quantity",
-                    discount: "$$product.discount",
-                    soldQuantity: {
-                        $let: {
-                            vars: {
-                                soldQuantities: "$items.soldQuantity",
-                                productIndex: { $indexOfArray: ["$items.productId", "$$product._id"] }
-                            },
-                            in: { $arrayElemAt: ["$$soldQuantities", "$$productIndex"] }
-                        }
+          $map: {
+            input: "$products",
+            as: "product",
+            in: {
+              _id: "$$product._id",
+              name: "$$product.name",
+              price: "$$product.price",
+              image: { $arrayElemAt: ["$$product.images", 0] },
+              quantity: "$$product.quantity",
+              discount: "$$product.discount",
+              soldQuantity: {
+                $let: {
+                  vars: {
+                    soldQuantities: "$items.soldQuantity",
+                    productIndex: {
+                      $indexOfArray: ["$items.productId", "$$product._id"]
                     }
+                  },
+                  in: {
+                    $arrayElemAt: ["$$soldQuantities", "$$productIndex"]
+                  }
                 }
+              }
             }
+          }
         }
-    }
+      }
 };
